989c2dd543df71c9f541ffc7c2c4d819d9f3e169
rafaelpardor <rafaelpardor@protonmail.com>
2020-08-11 02:14:56 -0400

    Hello World - Setting up client enviroment with react babel and webpack

---
+++ sf-movies/client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return(
+    <h1>Hello World</h1>
+  )
+}
+
+const root = document.getElementById('root');
+
+ReactDOM.render(<App />, root);
+
---
+++ sf-movies/client/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	// The point or points where to start the application bundling process
+	entry: './src/index.js',
+	output: {
+		// The output directory as an absolute path.
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']	
+	},
+	// Prevent webpack from parsing any files matching the given regular expression
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader'
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: './public/index.html',
+			filename: 'index.html'
+		})
+	]
+};
