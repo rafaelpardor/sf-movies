febc0871be8bb2119a56c5b51a5fec0ad8820ef0
rafaelpardor <rafaelpardor@protonmail.com>
2020-08-11 18:39:27 -0400

    Server Side - Adding 404 handler error

--- sf-movies/server/src/index.js
+++ sf-movies/server/src/index.js
@@ -4,7 +4,10 @@ const helmet = require('helmet');
 const cors = require('cors');
 require('dotenv').config()
 
+const middleware = require('./middlewares')
+
 const app = express();
+
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
@@ -12,10 +15,13 @@ app.use(cors({
   })
 );
 
-
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+// 404 Erorr handler
+app.use(middleware.notFound)
+app.use(middleware.errorHandler)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
