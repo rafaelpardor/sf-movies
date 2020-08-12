ee1209411e14b354d206781bafab02e176c3894a
rafaelpardor <rafaelpardor@protonmail.com>
2020-08-11 20:30:11 -0400

    Server side - Adding conection to Mongo

--- sf-movies/server/src/index.js
+++ sf-movies/server/src/index.js
@@ -2,11 +2,22 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config()
 
-const middleware = require('./middlewares')
+const middleware = require('./middlewares');
 
 const app = express();
+const db = mongoose.connection;
+mongoose.connect(process.env.MONGO_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+db.on('error', console.error.bind(console, 'Connection Error'));
+db.on('open', () =>{
+	console.log("Connected to the database.")
+})
 
 app.use(morgan('common'));
 app.use(helmet());
@@ -27,4 +38,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
   console.log(`Server running in port: ${PORT}`);
 });
-
