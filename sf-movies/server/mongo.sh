d0cbf64a374ce86151ca4181f1cedd0026cfba20
rafaelpardor <rafaelpardor@protonmail.com>
2020-08-15 01:05:33 -0400

    Dockerizing app

---
+++ sf-movies/server/mongo.sh
@@ -0,0 +1,1 @@
+docker run --rm -d -p 27017:27017 --name mongo-sf --network sf-movies mongo:4.4.0
---
+++ sf-movies/server/mongoex.sh
@@ -0,0 +1,1 @@
+docker run --rm -d -p 8081:8081 --network sf-movies -e ME_CONFIG_MONGODB_SERVER=mongo-sf mongo-express
